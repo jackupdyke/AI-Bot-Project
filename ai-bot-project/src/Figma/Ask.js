40abc20313ee31006081100a4bd1e055a4992ade
Max Buss <max.buss@neudesic.com>
2024-01-24 13:42:58 -0700

    fixing html and css for input box

--- ai-bot-project/src/Figma/Ask.js
+++ ai-bot-project/src/Figma/Ask.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect } from "react";
 import { useReducer } from "react";
+import submitIcon from './Icons/send.png';
 //import { IconComponentNode } from "./IconComponentNode";
 //import { NamecandidateNameSendScore101 } from "./NamecandidateNameSendScore101";
 import "./Ask.css";
@@ -9,70 +10,93 @@ export const Ask = ({
     property1,
     className,
 }) => {
-    const [state, dispatch] = useReducer(reducer, {
-        property1: property1 || "default",
-    });
+    // const [state, dispatch] = useReducer(reducer, {
+    //     property1: property1 || "default",
+    // });
+
+    const [inputValue, setInputValue] = React.useState("");
+    const [inputCharCount, setInputCharCount] = React.useState(0);
+
+    useEffect(() => {
+        console.log("inputValue", inputValue);
+        console.log("inputCharCount", inputCharCount);
+    }, [inputValue, inputCharCount]);
 
     return (
-        <div
-            className={`ask ${state.property1} ${className}`}
-            onMouseEnter={() => {
-                dispatch("mouse_enter");
-            }}
-            onMouseLeave={() => {
-                dispatch("mouse_leave");
-            }}
-        >
-            <div className="frame">
-                <input className="text-wrapper" type="text" name="name" />
-                {/* <div className="text-wrapper">Ask me anything...</div> */}
+        <>
+            <div
+                className={`ask  
+            ${className}`}
+            // onMouseEnter={() => {
+            //     dispatch("mouse_enter");
+            // }}
+            // onMouseLeave={() => {
+            //     dispatch("mouse_leave");
+            // }}
+            >
+                <div className="frame">
+                    <input
+                        className="ask text-wrapper"
+                        type="text"
+                        name="name"
+                        placeholder="Ask me anything..."
+                        onChange={(event) => {
+                            setInputValue(event.target.value);
+                            setInputCharCount(event.target.value.length);
+                        }}
+                    />
+                </div>
+
+                <div className="div">
+                    <div className="ask text-wrapper-2">{inputCharCount}/4000</div>
+                </div>
             </div>
-            <div className="div">
-                <div className="text-wrapper-2">0/4000</div>
+            <div className="submit">
+                <img className="submit-icon" src={submitIcon} />
             </div>
-        </div>
+        </>
     );
 };
 
-function reducer(state, action) {
-    if (state.property1 === "default") {
-        switch (action) {
-            case "mouse_enter":
-                return {
-                    property1: "hover",
-                };
-        }
-    }
+// function reducer(state, action) {
+//     if (state.property1 === "default") {
+//         switch (action) {
+//             case "mouse_enter":
+//                 return {
+//                     property1: "hover",
+//                 };
+//         }
+//     }
 
-    if (state.property1 === "hover") {
-        switch (action) {
-            case "mouse_leave":
-                return {
-                    property1: "default",
-                };
-        }
-    }
+//     if (state.property1 === "hover") {
+//         switch (action) {
+//             case "mouse_leave":
+//                 return {
+//                     property1: "default",
+//                 };
+//         }
+//     }
 
-    if (state.property1 === "light") {
-        switch (action) {
-            case "mouse_enter":
-                return {
-                    property1: "light-hover",
-                };
-        }
-    }
+//     if (state.property1 === "light") {
+//         switch (action) {
+//             case "mouse_enter":
+//                 return {
+//                     property1: "light-hover",
+//                 };
+//         }
+//     }
 
-    if (state.property1 === "light-hover") {
-        switch (action) {
-            case "mouse_leave":
-                return {
-                    property1: "light",
-                };
-        }
-    }
+//     if (state.property1 === "light-hover") {
+//         switch (action) {
+//             case "mouse_leave":
+//                 return {
+//                     property1: "light",
+//                 };
+//         }
+//     }
 
-    return state;
-}
+//     return state;
+// }
 
 Ask.propTypes = {
     property1: PropTypes.oneOf(["hover", "light-hover", "light", "default"]),
